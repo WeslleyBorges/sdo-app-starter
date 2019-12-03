945674621a4dd268c18cc0414885624ee0a62f4a
Weslley <weslleynfs@gmail.com>
2019-12-02 22:59:45 -0400

    Teste

--- src/app/security/login/login.service.ts
+++ src/app/security/login/login.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable'
 import { SDO_API } from "app/app.api";
-import { User } from "../../../../backend/users";
+// import { User } from "../../../../backend/users";
  
 @Injectable()
 export class LoginService{
@@ -11,7 +11,8 @@ export class LoginService{
 
 
     login(email: string, password: string): Observable<User>{
-        return this.http.post<User>(`${SDO_API}/login`,
-         {email: email, password: password})
+        // return this.http.post<User>(`${SDO_API}/login`,
+        //  {email: email, password: password})
+        return null
     }
 }
